ad5f4cc317953ff6d548552d6ed841706ce599cc
PedroS56 <pedrosalvador5699@gmail.com>
2020-10-28 18:07:06 -0600

    shell

--- shell.c
+++ shell.c
@@ -3,32 +3,36 @@
 #include <string.h>
 #include "tiempo.h"
 
+void Insercion(int *, int);
+
 
 //***********************
 //Metodo de ordenamiento shell
 //***********************
 int main (int argc, char* argv[])
 {	
-	if (argc != 2)
-    exit(1);
 		
+	double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
 
-
+	//**********************	
+	//Algoritmo
+	//**********************	
 	FILE *Numeros;  
    Numeros = fopen("numeros10millones.txt", "r");
-   
  
-	int n;
+	int n,i;
 	n=atoi(argv[1]);
 	int *arr;
+	//scanf("%d",&n);
 	arr = (int *) malloc(sizeof(int)*n);
 	
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
       fscanf(Numeros,"%d",&arr[i]);     
     fclose(Numeros);
 
-	int b,temp,x;
+	int k = n/2, b,temp,x;
 
+	uswtime(&utime0, &stime0, &wtime0);
 	
 	for(int i = n/2;i>=1;i/=2){
 		b=1;
@@ -45,11 +49,31 @@ int main (int argc, char* argv[])
 		}
 	}
 
+	uswtime(&utime1, &stime1, &wtime1);
 	
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		     printf("%d\n",arr[i]);  
 
 	}
+
+	//Cálculo del tiempo de ejecución del programa
+    printf("\n");
+	printf("real (Tiempo total)  %.10f s\n",  wtime1 - wtime0);
+	printf("user (Tiempo de procesamiento en CPU) %.10f s\n",  utime1 - utime0);
+	printf("sys (Tiempo en acciónes de E/S)  %.10f s\n",  stime1 - stime0);
+	printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+	printf("\n");
+	
+	//Mostrar los tiempos en formato exponecial
+	printf("\n");
+	printf("real (Tiempo total)  %.10e s\n",  wtime1 - wtime0);
+	printf("user (Tiempo de procesamiento en CPU) %.10e s\n",  utime1 - utime0);
+	printf("sys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
+	printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+	printf("\n");
+	//**********************
+
+	//Terminar programa normalmente	
 	exit (0);	
 }
