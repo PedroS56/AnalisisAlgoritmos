ef0e205ecef9d675b5bee6cd5a3e9b8ba43c2fcc
Ivette1111 <73195771+Ivette1111@users.noreply.github.com>
2020-10-29 13:22:13 -0600

    Add files via upload

---
+++ ScriptBurbuja.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+gcc Burbuja.c tiempo.c -o a
+
+./a 100 < numeros10M.txt >> Burbuja.txt
+
+./a 1000 < numeros10M.txt >> Burbuja.txt
+
+./a 5000 < numeros10M.txt >> Burbuja.txt
+
+./a 10000 < numeros10M.txt >> Burbuja.txt
+
+./a 30000 < numeros10M.txt >> Burbuja.txt
+
+./a 50000 < numeros10M.txt >> Burbuja.txt
+
+./a 100000 < numeros10M.txt >> Burbuja.txt
+
+./a 200000 < numeros10M.txt >> Burbuja.txt
+
+./a 300000 < numeros10M.txt >> Burbuja.txt
+
+./a 350000 < numeros10M.txt >> Burbuja.txt
+
+./a 400000 < numeros10M.txt >> Burbuja.txt
+
+./a 500000 < numeros10M.txt >> Burbuja.txt
+
+./a 550000 < numeros10M.txt >> Burbuja.txt
+
+./a 600000 < numeros10M.txt >> Burbuja.txt
+
+./a 700000 < numeros10M.txt >> Burbuja.txt
+
+./a 800000 < numeros10M.txt >> Burbuja.txt
+
+./a 900000 < numeros10M.txt >> Burbuja.txt
+
+./a 1000000 < numeros10M.txt >> Burbuja.txt
+
+./a 100 < numeros10M.txt >> Burbuja.txt
+
+./a 100 < numeros10M.txt >> Burbuja.txt
