4901ecdd9cca95cba038c7a0e7a77cbc311837ac
SGamaliel <aarongamasc@gmail.com>
2020-11-21 20:11:03 -0600

    Busqueda Fibonacci con hilos terminada, falta probarla

--- PRACTICA_2/BusquedaFibonacciHilos.c
+++ PRACTICA_2/BusquedaFibonacciHilos.c
@@ -1,15 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 struct argumentos {
-	int arreglo[];
+	int *arr;
 	int fib2;
 	int fib1;
-	int fibM;
 	int n;
 	int target;	
 };
 
-void *fibonacci(){
+int min(int x, int y) { return (x<=y)? x : y; }
+
+void *fibonacci(void *ags){
+	struct argumentos *args = (struct argumentos*) ags;
+	//struct argumentos *margs = NULL;
+	int status = 0, offset = 0;
+	/* Inizializa numeros fibonacci  */
+    int fibMMm2 = args->fib2; // (m-2)'th Fibonacci No. 
+    int fibMMm1 = args->fib1; // (m-1)'th Fibonacci No. 
+    int fibM = fibMMm2 + fibMMm1; // m Fibonacci 
+    int n = args->n;
+    int x = args->target;
+
+	while (fibM > 1) 
+        { 
+            // Check if fibMm2 is a valid location 
+            int i = min(offset+fibMMm2, n-1); 
+  
+            /* Si x es maqyor que el valor de index fibMn2, corta el subarray */
+            if ((args->arr[i]) < x) 
+            { 
+                fibM = fibMMm1; 
+                fibMMm1 = fibMMm2; 
+                fibMMm2 = fibM - fibMMm1; 
+                offset = i; 
+            } 
+  
+            /* Si x es menor que el valor index fibMm2, corta el subarray despues de i+1 */
+            else if ((args->arr[i]) > x) 
+            { 
+                fibM = fibMMm2; 
+                fibMMm1 = fibMMm1 - fibMMm2; 
+                fibMMm2 = fibM - fibMMm1; 
+            } 
+  
+            /* Elemento encontrado retorna S*/
+            else
+            {
+            	status = 0;
+    			pthread_exit((void*)&status);        	
+            } 
+        } 
+  
+        /* compara el ultimo elemento con x */
+        if(fibMMm1 == 1 && (args->arr[offset+1]) == x) 
+            status = 0;
+  
+        /*Elemento no encontrado retorna N */
+        status = -1;
+	pthread_exit((void*)&status);
+}
+
+void main(int argc, char *argv[]){
+	struct argumentos *margs = NULL;
+	pthread_t id1,id2;
+	int n = 0, target = 0, *numeros, i = 0;
+	int eid1 = -1, eid2 = -1;
+
+	int fibMMm2 = 0; // (m-2)'th Fibonacci No. 
+    int fibMMm1 = 1; // (m-1)'th Fibonacci No. 
+    int fibM = fibMMm2 + fibMMm1; // m Fibonacci 
 	
+	n = atoi(argv[1]);
+	target = atoi(argv[2]);
+
+	numeros = malloc(sizeof(int)*n);
+	margs = malloc(sizeof(struct argumentos)*1);
+
+	margs -> n = n;
+	margs -> target = target;
+	
+	if(numeros==NULL)
+		exit(2);
+	//Se leen los numeros desde la consola o un archivo
+	for(i=0;i<n;i++){
+		scanf("%i",&numeros[i]);
+	}
+
+	/*Generamos los numeros fibonacci, para el tamaño de problema
+	el índice máximo alcanzado en la serie fibonacci será el 36
+	por lo tanto, se propone la creacion de dos hilos, uno busca
+	en índice < 18 y otro en índice > 18*/
+	i = 0;
+	/* fibM va a guardar el mas pequeño*/
+    while (fibM < n) 
+    { 
+        fibMMm2 = fibMMm1; 
+        fibMMm1 = fibM; 
+        fibM = fibMMm2 + fibMMm1; 
+        i++;
+        if(i==18){
+        	//Lanzamos un hilo que busque aqui
+        	margs -> fib2 = fibMMm2;
+        	margs -> fib1 = fibMMm1;
+        	pthread_create(&id1,NULL,fibonacci,(void*)margs);
+        }
+    } 
+    //Alcanzo los ultimos numeros, lanzamos otro hilo para que busque por alla
+    margs -> fib2 = fibMMm2;
+	margs -> fib1 = fibMMm1;
+	pthread_create(&id2,NULL,fibonacci,(void*)margs);
+  	
+  	pthread_join(id1,(void*)&eid1);
+  	if(eid1==0)
+  		printf("Encontrado por hilo 1\n");
+  	pthread_join(id2,(void*)&eid2);
+  	if(eid2==0)
+  		printf("Encontrado por hilo 2\n");
+  	if(eid1!=0 && eid2!=0)
+  		printf("No encontrado\n");
+
+  	
+	return;
 }
